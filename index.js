580b6b49cd4039b3cf936a28f3284ae034378382
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2018-05-15 10:56:52 -0600

    added some solutions

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 // Create 2 variables and use getElementsByTagName to select the h1 and img tags.
 // Log the variables to the console, observe the results.
 
+var h1Tag = document.getElementsByTagName('h1')[0]
+console.log(h1Tag);
 
 // Create a variable and use getElementById to select the img tag.
 // Log the variable to the console, observe the result.
 
+var imgId = document.getElementById('mainpicture')
+console.log(imgId);
 
 // Create a variable and use getElementsByClassName to select the li tag.
 // Log the variable to the console, observe the result.
 
+var liClass = document.getElementsByClassName('bearname')[1]
+console.log(liClass);
 
 // Create at least 3 variables and use querySelector to select the h1, img, and li tags.
 // Try grabbing by class and id and tag name.
 // Log the variables to the console, observe the results.
 
 
+
 // Create at least 3 variables and use querySelectorAll to select the h1, img, and li tags.
 // Try grabbing by class and id and tag name.
 // Log the variables to the console, observe the results.
+
+
 
 // PART TWO
 // The time of the bears ruling things is over. Lets use DOM manipulation to give power
@@ -26,6 +35,12 @@
 // Lets start by changing the text of the H1 to read "CORGIS!"
 // Don't forget to select the h1 before trying to change the text
 
+document.body.onload = changeHeader;
+
+function changeHeader() {
+  var el = document.getElementsByTagName('h1')[0]
+  el.innerText = "CORGIS!"
+}
 
 // Do the same thing with the paragraph tag. It should read
 // "Two types of corgi lords Pembroke and Cardigan"
@@ -43,7 +58,7 @@
 
 // STRETCH IT OUT
 
-//  Use this portion to cretae more images of corgis or more corgi 
+//  Use this portion to cretae more images of corgis or more corgi
 // related things. Maybe you want to have a collab take over and add
 // cats or brids also taking over. Use this section to create new elements
 // And add then to our corgi page
